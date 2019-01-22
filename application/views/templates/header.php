8b8c1f40d2115f92791bd06441f0b02758b69082
dlakmal76 <damith88@gmail.com>
2019-01-23 03:04:07 +0530

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,26 +1,29 @@
 <html>
     <head>
         <title>EMS</title>
-        <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css"> 
+        <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.css">
         <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
     </head>
     <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <a class="navbar-brand" href="<?php echo base_url(); ?>">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarColor02">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="<?php echo base_url(); ?>">Home <span class="sr-only">(current)</span></a>
+        <ul class="nav nav-pills mr-auto">
+            <li class="nav-item">
+                <a class="nav-link active" href="<?php echo base_url(); ?>">Home </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="<?php echo base_url(); ?>posts">Posts</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url(); ?>login">User Login</a>
             </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
@@ -29,5 +32,14 @@
         </form>
     </div>
 </nav>
+<div class="jumbotron">
+  <h1 class="display-3">Event Management System</h1>
+  <p class="lead">Brife discrition about the web application</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
 
 <div class="container">
