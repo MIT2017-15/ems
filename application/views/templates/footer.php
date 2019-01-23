d8db284deb4053c0b06f8d3f022f8bd76b82b610
dlakmal76 <damith88@gmail.com>
2019-01-23 21:40:57 +0530

    23/01/2019-9:40pm

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,7 @@
-        </div>
+    </div>
+    <script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/popper.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/custom.js"></script>
     </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,36 +2,82 @@
     <head>
         <title>EMS</title>
         <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.css">
-        <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
+        <link rel="stylesheet" href="https://bootswatch.com/_assets/css/custom.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
     </head>
     <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="<?php echo base_url(); ?>">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
 
-    <div class="collapse navbar-collapse" id="navbarColor02">
-        <ul class="nav nav-pills mr-auto">
+    <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+      <div class="container">
+        <a href="<?php echo base_url(); ?>" class="navbar-brand">EMS</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav">
+          <!--
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" id="themes">Themes <span class="caret"></span></a>
+              <div class="dropdown-menu" aria-labelledby="themes">
+                <a class="dropdown-item" href="../default/">Default</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="../cerulean/">Cerulean</a>
+                <a class="dropdown-item" href="../cosmo/">Cosmo</a>
+                <a class="dropdown-item" href="../cyborg/">Cyborg</a>
+                <a class="dropdown-item" href="../darkly/">Darkly</a>
+                <a class="dropdown-item" href="../flatly/">Flatly</a>
+                <a class="dropdown-item" href="../journal/">Journal</a>
+                <a class="dropdown-item" href="../litera/">Litera</a>
+                <a class="dropdown-item" href="../lumen/">Lumen</a>
+                <a class="dropdown-item" href="../lux/">Lux</a>
+                <a class="dropdown-item" href="../materia/">Materia</a>
+                <a class="dropdown-item" href="../minty/">Minty</a>
+                <a class="dropdown-item" href="../pulse/">Pulse</a>
+                <a class="dropdown-item" href="../sandstone/">Sandstone</a>
+                <a class="dropdown-item" href="../simplex/">Simplex</a>
+                <a class="dropdown-item" href="../sketchy/">Sketchy</a>
+                <a class="dropdown-item" href="../slate/">Slate</a>
+                <a class="dropdown-item" href="../solar/">Solar</a>
+                <a class="dropdown-item" href="../spacelab/">Spacelab</a>
+                <a class="dropdown-item" href="../superhero/">Superhero</a>
+                <a class="dropdown-item" href="../united/">United</a>
+                <a class="dropdown-item" href="../yeti/">Yeti</a>
+              </div>
+            </li>
+        -->
+            
             <li class="nav-item">
-                <a class="nav-link active" href="<?php echo base_url(); ?>">Home </a>
+              <a class="nav-link" href="<?php echo base_url(); ?>">Home</a> 
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
+              <a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url(); ?>posts">Posts</a>
+              <a class="nav-link" href="<?php echo base_url(); ?>posts">Post</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url(); ?>login">User Login</a>
+              <a class="nav-link" href="<?php echo base_url(); ?>login">User Login</a>
             </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search">
-             <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-        </form>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" id="download">Food and Catering Management<span class="caret"></span></a>
+              <div class="dropdown-menu" aria-labelledby="download">
+                <a class="dropdown-item" href="<?php echo base_url(); ?>addIngredients">Add Ingredients</a>
+                <div class="dropdown-divider"></div>
+              </div>
+            </li>
+          </ul>
+
+          <!--<ul class="nav navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="https://wrapbootstrap.com/?ref=bsw" target="_blank">WrapBootstrap</a>
+            </li>
+          </ul>-->
+
+        </div>
+      </div>
     </div>
-</nav>
+<div class="container">
 <div class="jumbotron">
   <h1 class="display-3">Event Management System</h1>
   <p class="lead">Brife discrition about the web application</p>
@@ -42,4 +88,3 @@
   </p>
 </div>
 
-<div class="container">
